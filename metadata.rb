76f4fcb0858f4d1b5e11283b8dff8835c54383bd
Jesús Eduardo Clemens Chong <edu.clemens@gmail.com>
2018-04-20 14:37:57 -0700

    Debian 8

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "ops@mojolingo.com"
 license          "Apache 2.0"
 description      "Installs/Configures FreeSWITCH"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 recipe "freeswitch", "Install FreeSWITCH and configure"
 
